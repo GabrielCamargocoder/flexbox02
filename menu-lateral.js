7fc5a087b7d2d8a11c6632f305bae03efbe16ac1
GabrielCamargocoder <gacamargooliveira@gmail.com>
2025-06-18 12:55:44 -0300

    configurando menu vertical

--- menu-lateral.js
+++ menu-lateral.js
@@ -1,17 +1,6 @@
 var itemMenu = document.querySelectorAll ('.item-menu')
 
-function selectLink(){
-     
-    itemMenu.forEach((item) =>
-        item.classList.remove('ativo')
-    )
-        this.classList.add('ativo')
 
-}
-
-itemMenu.forEach((item)=>
-    item.addEventListener('click',selectLink)
-)
 
 //Menu Expandir
 
